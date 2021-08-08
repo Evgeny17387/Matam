a64f5f4e280a98ec601a82af9267b64267c68cf5
evgeny17387 <evgeny17387@gmail.com>
2021-08-08 19:34:01 +0300

    ex 0, part 2

---
+++ ex0/part2/build/CMakeFiles/mtm_buggy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtm_buggy.dir/mtm_buggy.c.obj"
+  "CMakeFiles/mtm_buggy.dir/mtm_buggy.c.obj.d"
+  "libmtm_buggy.dll.a"
+  "mtm_buggy.exe"
+  "mtm_buggy.exe.manifest"
+  "mtm_buggy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mtm_buggy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ex0/part2/test.bat
@@ -0,0 +1,4 @@
+cls
+
+.\build\mtm_buggy.exe < .\test1.in > .\test1_private.out
+.\build\mtm_buggy.exe < .\test2.in > .\test2_private.out
