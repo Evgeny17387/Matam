84c839e100bf06cbde8bd76628bdf87c0d0fe681
evgeny17387 <evgeny17387@gmail.com>
2021-08-21 20:30:00 +0300

    add more tests to MAP

--- ex1/tests/map_example_test.c
+++ ex1/tests/map_example_test.c
@@ -1,8 +1,12 @@
 #include "../test_utilities.h"
 #include <stdlib.h>
 #include "../mtm_map/map.h"
+#include <string.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
 
-#define NUMBER_TESTS 15
+#define NUMBER_TESTS 22
 
 /** Function to be used for copying an int as a key to the map */
 static MapKeyElement copyKeyInt(MapKeyElement n) {
@@ -43,6 +47,48 @@ static void freeChar(MapDataElement n) {
 /** Function to be used by the map for comparing elements */
 static int compareInts(MapKeyElement n1, MapKeyElement n2) {
     return (*(int *) n1 - *(int *) n2);
+}
+
+/** Function to be used for copying an int as a key to the map */
+static MapKeyElement copyKeyString(MapKeyElement n) {
+    if (!n) {
+        return NULL;
+    }
+    char* copy = (char*)malloc(strlen(n) + 1);
+   if (!copy) {
+       return NULL;
+    }
+    copy = strcpy(copy,n);
+    return (MapKeyElement) copy;
+}
+
+/** Function to be used for copying a char as a data to the map */
+static MapDataElement copyDataString(MapDataElement n) {
+    if (!n) {
+        return NULL;
+    }
+    char* copy = (char*)malloc(strlen(n) + 1);
+    if (!copy) {
+        return NULL;
+    }
+    copy = strcpy(copy,n);
+    return (MapDataElement) copy;
+}
+
+/** Function to be used by the map for freeing elements */
+static void freeKeyString(MapKeyElement n) {
+    free(n);
+}
+
+/** Function to be used by the map for freeing elements */
+static void freeDataString(MapDataElement n) {
+    free(n);
+}
+
+/** Function to be used by the map for comparing elements */
+static int compareStrings(MapKeyElement n1, MapKeyElement n2) {
+
+    return (strcmp(n1,n2));
 }
 
 bool testMapCreateDestroy()
@@ -689,6 +735,280 @@ bool nullTests()
 	return true;
 }
 
+bool testMapCreate() {
+    Map map1 = mapCreate(copyDataString, copyKeyString, freeKeyString,freeDataString ,  compareStrings);;
+    Map map2 = mapCreate(copyDataString, copyKeyString, freeKeyString,freeDataString ,  compareStrings);;
+    Map map3 = mapCreate(copyDataString, copyKeyString, freeKeyString,freeDataString ,  compareStrings);;
+    mapDestroy(map3);
+    ASSERT_TEST(map1 != NULL);
+    ASSERT_TEST(map2 != NULL);
+    ASSERT_TEST(mapGetSize(map1) == 0);
+    ASSERT_TEST(mapGetFirst(map1) == NULL);
+    mapPut(map1, "mary", "poppins");
+    mapDestroy(map1);
+    map1 = NULL;
+    mapDestroy(map1);
+    ASSERT_TEST(mapGet(map1, "mary") == NULL);
+    ASSERT_TEST(mapGetSize(map1) == -1);
+    ASSERT_TEST(map2 != NULL);
+    mapDestroy(map2);
+    return true;
+}
+
+bool testMapPutGet() {
+    Map map = mapCreate(copyDataString, copyKeyString, freeKeyString,freeDataString ,  compareStrings);;
+
+     char *key1 = "key1";
+     char *key2 = "key2";
+     char *key3 = "key3";
+     char *value1 = "value1";
+    char *value2 = "value2";
+     char *value3 = "value3";
+     char *value4 = "value4";
+
+    int count = 0;
+
+    ASSERT_TEST(mapPut(map, key1, value1) == MAP_SUCCESS);
+    count++;
+   // char* str1=(char*)mapGet(map, &key2), value2);
+   // ASSERT_TEST(strcmp(mapGet(map, key1), value1) == 0);
+    ASSERT_TEST(mapPut(map, key2, value2) == MAP_SUCCESS);
+    count++;
+   // ASSERT_TEST(strcmp((char*)mapGet(map, key2), value2) == 0);
+    ASSERT_TEST(mapPut(map, key3, value3) == MAP_SUCCESS);
+    count++;
+
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(mapPut(map, key3, value4) == MAP_SUCCESS);
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(mapRemove(map, key2) == MAP_SUCCESS);
+    count--;
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(mapRemove(map, key1) == MAP_SUCCESS);
+    count--;
+    ASSERT_TEST(mapRemove(map, key3) == MAP_SUCCESS);
+    count--;
+    ASSERT_TEST(mapPut(map, key3, value1) == MAP_SUCCESS);
+    count++;
+
+    ASSERT_TEST(mapPut(map, key3, NULL) == MAP_NULL_ARGUMENT);
+    ASSERT_TEST(mapPut(map, NULL, value3) == MAP_NULL_ARGUMENT);
+    ASSERT_TEST(mapPut(map, NULL, NULL) == MAP_NULL_ARGUMENT);
+    ASSERT_TEST(mapPut(NULL, key3, value3) == MAP_NULL_ARGUMENT);
+    ASSERT_TEST(mapPut(NULL, NULL, NULL) == MAP_NULL_ARGUMENT);
+    ASSERT_TEST(mapGetSize(map) == count);
+
+    ASSERT_TEST(mapPut(map, key1, value2) == MAP_SUCCESS);
+    count++;
+    ASSERT_TEST(strcmp(mapGet(map, key1), value2) == 0);
+    ASSERT_TEST(mapPut(map, key1, value1) == MAP_SUCCESS);
+    ASSERT_TEST(strcmp(mapGet(map, key1), value1) == 0);
+    ASSERT_TEST(mapRemove(map, key1) == MAP_SUCCESS);
+    count--;
+    ASSERT_TEST(mapGet(map, key1) == NULL);
+
+    ASSERT_TEST(mapPut(map, key1, value1) == MAP_SUCCESS);
+    count++;
+    ASSERT_TEST(mapGet(map, key1) != value1);
+    ASSERT_TEST(mapGetSize(map) == count);
+
+    ASSERT_TEST(mapGet(map, key1) == mapGet(map, key1));
+
+    ASSERT_TEST(mapPut(map, "key1", value1) == MAP_SUCCESS);
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(strcmp(mapGet(map, key1), value1) == 0);
+
+    ASSERT_TEST(mapPut(map, key1, "") == MAP_SUCCESS);
+    ASSERT_TEST(mapPut(map, key2, "") == MAP_SUCCESS);
+    count++;
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(mapPut(map, "", value1) == MAP_SUCCESS);
+    count++;
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(strcmp(mapGet(map, ""), value1) == 0);
+    ASSERT_TEST(strcmp(mapGet(map, key1), "") == 0);
+
+    char *super_long_string =
+    "Hi— I’m Ted Mosby. And exactly 45 days from now you and I are "
+            "going to meet and we’re going to fall in love and we’re going to "
+            "get married and we’re going to have 2 kids and we’re going to "
+            "love them and each other so much. All that is 45 days away, but "
+            "I’m here now I guess because… I want those extra 45 days with "
+            "you. I want each one of them. Look and if I can’t have them I’ll "
+            "take the 45 seconds before your boyfriend shows up and punches me "
+            "in the face, because… I love you. I’m always gonna love you, til "
+            "the end of my days and beyond. You’ll see.Hi— I’m Ted Mosby. And "
+            "exactly 45 days from now you and I are "
+            "going to meet and we’re going to fall in love and we’re going to "
+            "get married and we’re going to have 2 kids and we’re going to "
+            "love them and each other so much. All that is 45 days away, but "
+            "I’m here now I guess because… I want those extra 45 days with "
+            "you. I want each one of them. Look and if I can’t have them I’ll "
+            "take the 45 seconds before your boyfriend shows up and punches me "
+            "in the face, because… I love you. I’m always gonna love you, til "
+            "the end of my days and beyond. You’ll see.Hi— I’m Ted Mosby. And "
+            "exactly 45 days from now you and I are "
+            "going to meet and we’re going to fall in love and we’re going to "
+            "get married and we’re going to have 2 kids and we’re going to "
+            "love them and each other so much. All that is 45 days away, but "
+            "I’m here now I guess because… I want those extra 45 days with "
+            "you. I want each one of them. Look and if I can’t have them I’ll "
+            "take the 45 seconds before your boyfriend shows up and punches me "
+            "in the face, because… I love you. I’m always gonna love you, til "
+            "the end of my days and beyond. You’ll see.Hi— I’m Ted Mosby. And "
+            "exactly 45 days from now you and I are "
+            "going to meet and we’re going to fall in love and we’re going to "
+            "get married and we’re going to have 2 kids and we’re going to "
+            "love them and each other so much. All that is 45 days away, but "
+            "I’m here now I guess because… I want those extra 45 days with "
+            "you. I want each one of them. Look and if I can’t have them I’ll "
+            "take the 45 seconds before your boyfriend shows up and punches me "
+            "in the face, because… I love you. I’m always gonna love you, til "
+            "the end of my days and beyond. You’ll see.Hi— I’m Ted Mosby. And "
+            "exactly 45 days from now you and I are "
+            "going to meet and we’re going to fall in love and we’re going to "
+            "get married and we’re going to have 2 kids and we’re going to "
+            "love them and each other so much. All that is 45 days away, but "
+            "I’m here now I guess because… I want those extra 45 days with "
+            "you. I want each one of them. Look and if I can’t have them I’ll "
+            "take the 45 seconds before your boyfriend shows up and punches me "
+            "in the face, because… I love you. I’m always gonna love you, til "
+            "the end of my days and beyond. You’ll see.";
+    ASSERT_TEST(mapPut(map, key1, super_long_string) == MAP_SUCCESS);
+    ASSERT_TEST(strcmp(mapGet(map, key1), super_long_string) == 0);
+    ASSERT_TEST(mapPut(map, super_long_string, value1) == MAP_SUCCESS);
+    ASSERT_TEST(strcmp(mapGet(map, super_long_string), value1) == 0);
+    count++;
+    ASSERT_TEST(strcmp(mapGet(map, super_long_string), value1) == 0);
+    ASSERT_TEST(mapGetSize(map) == count);
+    ASSERT_TEST(strcmp(mapGet(map, key2), "") == 0);
+    mapDestroy(map);
+    return true;
+}
+
+bool testMapCopy() {
+    ASSERT_TEST(mapCopy(NULL) == NULL);
+    Map map1 = mapCreate(copyDataString, copyKeyString, freeKeyString,freeDataString ,  compareStrings);;
+    ASSERT_TEST(mapPut(map1, "key1", "value1") == MAP_SUCCESS);
+    ASSERT_TEST(mapPut(map1, "key2", "value2") == MAP_SUCCESS);
+    Map map2 = mapCopy(map1);
+    ASSERT_TEST(strcmp(mapGet(map2, "key1"), mapGet(map1, "key1")) == 0);
+    ASSERT_TEST(strcmp(mapGet(map2, "key2"), mapGet(map1, "key2")) == 0);
+
+    ASSERT_TEST(mapGetSize(map2) == mapGetSize(map1));
+
+    ASSERT_TEST(mapGet(map2, "key2") != mapGet(map1, "key2"));
+    ASSERT_TEST(mapRemove(map1, "key1") == MAP_SUCCESS);
+
+    // Try re-allocating the same memory so it will change
+    ASSERT_TEST(mapPut(map1, "key4", "value1") == MAP_SUCCESS);
+    ASSERT_TEST(mapPut(map1, "key5", "value1") == MAP_SUCCESS);
+    ASSERT_TEST(mapPut(map1, "key6", "value1") == MAP_SUCCESS);
+    ASSERT_TEST(mapPut(map1, "key7", "value1") == MAP_SUCCESS);
+    ASSERT_TEST(mapGet(map2, "key2") != NULL);
+
+    mapDestroy(map1);
+    ASSERT_TEST(strcmp(mapGet(map2, "key1"), "value1") == 0);
+    ASSERT_TEST(strcmp(mapGet(map2, "key2"), "value2") == 0);
+    mapDestroy(map2);
+    return true;
+}
+
+bool testMapGetSize() {
+    Map map = mapCreate(copyDataString, copyKeyString, freeKeyString,freeDataString ,  compareStrings);;
+
+    ASSERT_TEST(mapGetSize(map) == 0);
+    ASSERT_TEST(mapPut(map, "key1", "value1") == MAP_SUCCESS);
+    ASSERT_TEST(mapPut(map, "key1", "value2") == MAP_SUCCESS);
+    ASSERT_TEST(mapGetSize(map) == 1);
+    ASSERT_TEST(mapRemove(map, "key1") == MAP_SUCCESS);
+    ASSERT_TEST(mapGetSize(map) == 0);
+    ASSERT_TEST(mapGetSize(NULL) == -1);
+    mapDestroy(map);
+    return true;
+}
+
+bool testMapContains() {
+    Map map1 = mapCreate(copyDataString,copyKeyString,  freeKeyString,freeDataString,  compareStrings);;
+    mapPut(map1, "key1", "value1");
+    ASSERT_TEST(mapContains(map1, "key1") == true);
+    ASSERT_TEST(mapContains(NULL, "key1") == false);
+    ASSERT_TEST(mapContains(map1, NULL) == false);
+    ASSERT_TEST(mapContains(map1, "dani") == false);
+    mapDestroy(map1);
+    return true;
+}
+
+bool testMapRemove() {
+    Map map1 = mapCreate(copyDataString,copyKeyString, freeKeyString,freeDataString,  compareStrings);;
+    mapPut(map1, "key1", "value1");
+    mapPut(map1, "key2", "value2");
+    mapPut(map1, "key3", "value3");
+    mapRemove(map1, "key1");
+    ASSERT_TEST(mapContains(map1, "key1") == false);
+    ASSERT_TEST(mapGetSize(map1) == 2);
+    mapPut(map1, "key1", "value1");
+    mapRemove(map1, "key3");
+    ASSERT_TEST(mapContains(map1, "key3") == false);
+    ASSERT_TEST(mapGetSize(map1) == 2);
+    mapPut(map1, "key3", "value3");
+    mapRemove(map1, "key3");
+    ASSERT_TEST(mapContains(map1, "key3") == false);
+    ASSERT_TEST(mapGetSize(map1) == 2);
+    mapDestroy(map1);
+    return true;
+}
+
+bool testMapForEach() {
+    Map map = mapCreate(copyDataString,copyKeyString,  freeKeyString,freeDataString,  compareStrings);;
+    mapPut(map, "key 1", "value 1");
+    mapPut(map, "key 2", "value 2");
+    mapPut(map, "key 3", "value 3");
+    mapPut(map, "key 4", "value 4");
+    mapPut(map, "key 5", "value 5");
+    int arr[5] = {0};
+    int i = 0;
+    //checking that the MAP_FOREACH went through all of the keys one at a time
+    MAP_FOREACH(int *,key, map) {
+        arr[i]++;
+        i++;
+        freeKeyString(key);
+    }
+    for (int j = 0; j < 5; j++){
+        ASSERT_TEST(arr[j] == 1);
+    }
+    //trying to iterate over a NULL argument
+    bool something_happened = false;
+    MAP_FOREACH(int *, key, NULL) {
+        ASSERT_TEST(key == NULL);
+        freeKeyString(key);
+        something_happened = true;
+    }
+    ASSERT_TEST(something_happened == false);
+
+    //trying to iterate over a empty map
+    Map map_2 = mapCreate(copyDataString,copyKeyString, freeKeyString,freeDataString,  compareStrings);;
+    MAP_FOREACH(int *, key, map_2) {
+        ASSERT_TEST(key == NULL);
+        freeKeyString(key);
+        something_happened = true;
+    }
+    ASSERT_TEST(something_happened == false);
+    MAP_FOREACH(int *, key,map){
+        mapPut(map_2,key,mapGet(map,key));
+        freeKeyString(key);
+    }
+    ASSERT_TEST(mapGetSize(map_2) == mapGetSize(map));
+    MAP_FOREACH(int *, key,map_2){
+        ASSERT_TEST(mapGet(map,key)!=NULL);
+        freeKeyString(key);
+    }
+    
+    mapDestroy(map);
+    mapDestroy(map_2);
+    return true;
+}
+
 /*The functions for the tests should be added here*/
 bool (*tests[]) (void) = {
         testMapCreateDestroy,
@@ -705,7 +1025,14 @@ bool (*tests[]) (void) = {
 		testClear,
 		testCopy,
 		testContains,
-		nullTests
+		nullTests,
+        testMapPutGet,
+        testMapCopy,
+        testMapGetSize,
+        testMapCreate,
+        testMapContains,
+        testMapRemove,
+        testMapForEach
 };
 
 /*The names of the test functions should be added here*/
@@ -724,7 +1051,14 @@ const char* testNames[] = {
 		"testClear",
 		"testCopy",
 		"testContains",
-		"nullTests"
+		"nullTests",
+        "testMapPutGet",
+        "testMapCopy",
+        "testMapGetSize",
+        "testMapCreate",
+        "testMapContains",
+        "testMapRemove",
+        "testMapForEach"
 };
 
 int main(int argc, char *argv[]) {
