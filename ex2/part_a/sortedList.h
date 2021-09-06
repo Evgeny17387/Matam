1d424639d1d0b3695a32834d2c98413a782a6742
Evgeny Ostrovsky <evostrov@microsoft.com>
2021-09-06 14:37:40 +0300

    first implementation for insert

--- ex2/part_a/sortedList.h
+++ ex2/part_a/sortedList.h
@@ -10,9 +10,16 @@ namespace mtm
     {
     private:
 
+        // ToDo: consider replacing with class
+        struct node
+        {
+            T data;
+            node *next;
+        };
+
         int size;
 
-        T t;
+        node *head;
 
     public:
 
@@ -25,34 +32,87 @@ namespace mtm
 
         void insert(const T& t);
 
+        // class Iterator;
+        // Itereator begin() const;
+        // Itereator end() const;
+
+        // template <class T>
+        // class SortedList<T>::Iterator
+        // {
+        // }
+
+        class MethodNotImplementedYet{};
     };
 
     template <class T>
     SortedList<T>::SortedList()
     {
+        this->head = NULL;
         this->size = 0;
     }
 
     template <class T>
     SortedList<T>::~SortedList()
     {
+        node *node_pointer = this->head;
+
+        // ToDo: replace with itertor
+        while (NULL != node_pointer)
+        {
+            node *temp = node_pointer->next;
+            delete node_pointer;
+            node_pointer = temp;
+        }
+
+        this->head = NULL;
     }
 
     template <class T>
     SortedList<T>::SortedList(const SortedList& sortedList)
     {
+        throw MethodNotImplementedYet();
     }
 
     template <class T>
     SortedList<T>& SortedList<T>::operator=(const SortedList& sortedList)
     {
+        throw MethodNotImplementedYet();
     }
 
     template <class T>
     void SortedList<T>::insert(const T& t)
     {
-        this->t = t;
+        node *node_pointer_new = new node;
+        node_pointer_new->next = NULL;
+        node_pointer_new->data = t;
+
+        if (NULL == this->head)
+        {
+            this->head = node_pointer_new;
+            return;
+        }
+
+        // ToDo: implemented as sorted list
+        node *node_pointer = this->head;
+        while (NULL != node_pointer->next)
+        {
+            node_pointer = node_pointer->next;
+        }
+
+        node_pointer->next = node_pointer_new;
     }
+
+    // template <class T>
+    // void SortedList<T>::begin() const
+    // {
+    //     return Iterator(this, 0);
+    // }
+
+    // template <class T>
+    // void SortedList<T>::end() const
+    // {
+    //     return Iterator(this, 0);
+    // }
 }
 
 #endif //_SORTED_LIST
